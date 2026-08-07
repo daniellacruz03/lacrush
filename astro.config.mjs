3444c878526551796ba3de2ce397d64a1b41c834
Dani <daniellacruznieto@gmail.com>
2026-08-07 16:16:42 -0400

    Configure base URL and assets path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://daniellacruz03.github.io',
+  base: '/lacrush',
+});
+
